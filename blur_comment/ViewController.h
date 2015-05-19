8518b161478cf1fe87f87abd6233dad0834e1c55
dai.fengyi <fengji@zkjinshi.com>
2015-05-19 08:59:12 +0800

    first import

---
+++ blur_comment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blur_comment
+//
+//  Created by dai.fengyi on 15/5/19.
+//  Copyright (c) 2015年 childrenOurFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ blur_comment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  blur_comment
+//
+//  Created by dai.fengyi on 15/5/19.
+//  Copyright (c) 2015年 childrenOurFuture. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
